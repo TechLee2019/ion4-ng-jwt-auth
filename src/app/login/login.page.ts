4a5b535003ad175f1445e19f2487805c75a29b46
WenchengLi-WL <babystar6666@outlook.com>
2019-12-16 18:18:07 +0900

    Creating the Public & Protected App Pages

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { async } from '@angular/core/testing';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+    credentials = {
+        email: 'ali@david.com',
+        pwd: '123'
+    };
 
-  constructor() { }
+    constructor(
+        private auth: AuthService,
+        private router: Router,
+        private alertCtrl: AlertController
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    login()
+    {
+        this.auth.login(this.credentials).subscribe(async res => {
+            if (res) {
+                this.router.navigateByUrl('/members');
+            } else {
+                const alert = await this.alertCtrl.create({
+                    header: 'Login Failed',
+                    message: 'Wrong credentials',
+                    buttons: ['OK']
+                });
+                alert.present();
+            }
+        });
+    }
 
 }
